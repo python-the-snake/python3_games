af95ed60a5bcf6a575a001301176a02901f189e3
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-07-14 12:58:00 +0300

    optimized file structure

--- tetris.py
+++ tetris.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-import sys, random
-from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
+import numba
+import random
+import sys
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 
 
 class Tetris(QMainWindow):
